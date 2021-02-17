147b3ce057ab2b5ecfdc6790af7e1ea5d86638c5
jiho9505 <sun_days@naver.com>
2021-02-17 18:47:50 +0900

    Added TSpart about page

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { Component } from './components/component.js';
+
+
+class App {
+  private readonly page: Component & Composable;
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
+    this.page.attachTo(appRoot);
+}
+
+new App(document.querySelector('.document')! as HTMLElement, document.body);
